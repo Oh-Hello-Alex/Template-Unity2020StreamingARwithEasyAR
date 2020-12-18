cc0736f3944d3b0ffbbd10d4077c2607e3b30ba2
Alex Serpentini <alexserpentini@protonmail.com>
2020-12-18 17:33:50 +0700

    first commit, empty project and gitignore

---
+++ Assets/PauseWhenInactive.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+
+public class PauseWhenInactive : MonoBehaviour
+{
+    public GameObject mediaPlayer;
+    public GameObject Trigger;
+
+    // exactly what it says on the tin: this pauses the media player when the trigger is inactive. 
+    void Update()
+    {
+        if (Trigger.activeSelf == false)
+		{
+            mediaPlayer.GetComponent<MediaPlayer>().Pause();  
+        }
+    }
+}
